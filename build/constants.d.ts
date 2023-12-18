7793e6910831d6c84720aac17f4f970b75bbef3c
Lim Jiechao <jiechao.lim@aboitiz.com>
2023-12-18 15:35:14 +0800

    feat: new build

---
+++ build/constants.d.ts
@@ -0,0 +1,5 @@
+export declare const ACCEPT_HEADER = "Accept";
+export declare const CONTENT_TYPE_HEADER = "Content-Type";
+export declare const CONTENT_TYPE_JSON = "application/json";
+export declare const CONTENT_TYPE_GQL = "application/graphql-response+json";
+//# sourceMappingURL=constants.d.ts.map
---
+++ build/constants.js
@@ -0,0 +1,5 @@
+export const ACCEPT_HEADER = `Accept`;
+export const CONTENT_TYPE_HEADER = `Content-Type`;
+export const CONTENT_TYPE_JSON = `application/json`;
+export const CONTENT_TYPE_GQL = `application/graphql-response+json`;
+//# sourceMappingURL=constants.js.map
